2d372ab6133a7d5bf7ca41680cbb731b1aca8e58
Zak Payne <zakpayne8283@gmail.com>
2020-06-22 17:58:10 -0400

    created basic connection class

---
+++ pyball_package/connection/connection.py
@@ -0,0 +1,6 @@
+class Connection:
+
+    def __init__(self):
+        # Sets up the connection w/ parameters
+        # TODO: determine parameters
+        pass
--- pyball_package/main.py
+++ pyball_package/main.py
@@ -1,5 +1,7 @@
 class PyballAPI:
 
+
+
     def __init__(self):
         # TODO: create connection
         pass
