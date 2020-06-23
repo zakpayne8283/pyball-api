d6234f8066875924e615d657c10a6077b967b256
Zak Payne <zakpayne8283@gmail.com>
2020-06-22 21:20:56 -0400

    small changes to connection.py init

--- pyball_package/connection/connection.py
+++ pyball_package/connection/connection.py
@@ -1,8 +1,15 @@
 class Connection:
 
-    host_url = 'http://lookup-service-prod.mlb.com'
+    host_url = None
+    request_path = None
+    request_data = None
 
     def __init__(self):
         # Sets up the connection w/ parameters
+
+        self.host_url = 'http://lookup-service-prod.mlb.com'
+        self.request_path = '/json/named.{endpoint}.bam'
         # TODO: determine parameters
+
+    def make_request(self):
         pass
